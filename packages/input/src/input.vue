d60a206d3c063e82386bf2d6a90e45868b7b504f
Gaëtan Simon <gaetan.simon@jack.agency>
2022-04-04 19:22:18 +0200

    Input: don't compute height on textarea resize

--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -20,6 +20,7 @@
       <div class="el-input-group__prepend" v-if="$slots.prepend">
         <slot name="prepend"></slot>
       </div>
+
       <input
         :tabindex="tabindex"
         v-if="type !== 'textarea'"
@@ -84,6 +85,7 @@
         <slot name="append"></slot>
       </div>
     </template>
+
     <textarea
       v-else
       :tabindex="tabindex"
@@ -97,13 +99,12 @@
       :disabled="inputDisabled"
       :readonly="readonly"
       :autocomplete="autoComplete || autocomplete"
-      :style="textareaStyle"
       @focus="handleFocus"
       @blur="handleBlur"
       @change="handleChange"
       :aria-label="label"
-    >
-    </textarea>
+    />
+
     <span v-if="isWordLimitVisible && type === 'textarea'" class="el-input__count">{{ textLength }}/{{ upperLimit }}</span>
   </div>
 </template>
@@ -261,7 +262,7 @@
 
     watch: {
       value(val) {
-        this.$nextTick(this.resizeTextarea);
+        // this.$nextTick(this.resizeTextarea);
         if (this.validateEvent) {
           this.dispatch('ElFormItem', 'el.form.change', [val]);
         }
@@ -278,7 +279,7 @@
       type() {
         this.$nextTick(() => {
           this.setNativeInputValue();
-          this.resizeTextarea();
+          // this.resizeTextarea();
           this.updateIconOffset();
         });
       }
@@ -426,7 +427,7 @@
 
     mounted() {
       this.setNativeInputValue();
-      this.resizeTextarea();
+      // this.resizeTextarea();
       this.updateIconOffset();
     },
 
