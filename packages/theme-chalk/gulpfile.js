1086e37134dec7b9c82ba0734141f5de890d93c4
Gaëtan Simon <gaetan.simon@jack.agency>
2023-07-03 10:27:36 +0200

    build theme

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -4,9 +4,28 @@ const { series, src, dest } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
+const map = require('map-stream');
+const { Buffer } = require('node:buffer');
+
+const VAR_RE = /\/\/\/VAR (\@import\s["'][\w\.\/\-]+["'];)/i;
 
 function compile() {
   return src('./src/*.scss')
+    .pipe(map(function(file, cb) {
+      if (file.isNull()) {
+        return cb(null, file);
+      }
+
+      let contents = Buffer.from(file.contents, 'utf-8').toString('utf-8');
+      const importsVars = VAR_RE.exec(contents.slice(0, 1024));
+
+      if (Array.isArray(importsVars) && importsVars.length && importsVars[1]) {
+        contents = contents.replace(VAR_RE, importsVars[1]);
+        file.contents = Buffer.from(contents, 'utf8');
+      }
+
+      cb(null, file);
+    }))
     .pipe(sass.sync())
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
